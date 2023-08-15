00b3230ade928547683ed8fcade2c2c1152a49c0
Garneek <50029133+Garneek@users.noreply.github.com>
2023-08-15 21:21:09 +0200

    add comments to button.c

--- source/button.c
+++ source/button.c
@@ -2,7 +2,7 @@
 #include "button.h"
 #include "constants.h"
 
-
+//init button struct given its position and number of sprite in spritesheet defining its graphics
 Button init_button(u8 x, u8 y, u8 id){
   Button button;
   button.x = x;
@@ -12,17 +12,20 @@ Button init_button(u8 x, u8 y, u8 id){
 	return button;
 }
 
+//change button graphics; takes in sprite number in spritesheet defining new graphics
 void update_button_offset(Button* button, u8 new_offset){
   update_sprite_offset(&button->sprite, new_offset);
 }
 
-
+//draw button sprite; Takes in OAM number to be set
 void draw_button(Button *button, u8 id){
   if(!button->disabled)
     draw_sprite(&button->sprite, id, button->x, button->y);
 }
 
+//checks if button is checked given position that is touched
 bool is_button_pressed(Button *button, u8 x, u8 y){
+  if(button->disabled)return false;
   if(x < button->x)return false;
   if(x > button->x + BUTTON_SIZE)return false;
   if(y < button->y)return false;
@@ -30,10 +33,12 @@ bool is_button_pressed(Button *button, u8 x, u8 y){
   return true;
 }
 
+//disable button processing and rendering 
 void disable_button(Button*button){
   button->disabled = true;
 }
 
+//enable button processing and rendering 
 void enable_button(Button*button){
   button->disabled = false;
 }
